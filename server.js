985297b20232e5c1d5cf3c085502d9695efa9850
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2024-11-24 19:35:02 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,22 +5,26 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout',  'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 console.log('Database URL:', process.env.DATABASE_URL);
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true  })
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
-db.on('error', error => console.error(error))
+db.on('error', error => console.error('Error connecting to MongoDB:',error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
