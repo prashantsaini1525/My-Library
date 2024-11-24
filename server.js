ed604aab1ac79ad21e4c7d9b4a7d3a12f4762e04
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2024-11-24 21:34:13 +0530

    Server.js Updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.set('views', path.join(__dirname, '/views'))
 app.set('layout',  'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
