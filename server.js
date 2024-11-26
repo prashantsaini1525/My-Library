b50dac292e3c249c8e27d758afc3771848631c17
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2024-11-26 20:43:37 +0530

    Create/Index/New

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
-const path = require('path')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', __dirname + '/views')
 app.set('layout',  'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
 
 const mongoose = require('mongoose')
 if (!process.env.DATABASE_URL) {
@@ -42,8 +44,11 @@ const db = mongoose.connection
 db.on('error', error => console.error('Error connecting to MongoDB:',error))
 // db.once('open', () => console.log('Connected to Mongoose'))
 
+
+
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 
 const PORT = process.env.PORT || 3000;
