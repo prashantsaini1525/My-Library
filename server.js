93c67903929a905e5f163121c370ffef66fed4b4
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2024-12-04 22:46:25 +0530

    vercel

--- server.js
+++ server.js
@@ -42,7 +42,4 @@ app.use('/authors', authorRouter)
 app.use('/books', bookRouter)
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+module.exports = app;
